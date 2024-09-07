76a6f61ca8865c78ef73a1ab26aa8057f73b8bbf
bysunfruto <163089667+bysunfruto@users.noreply.github.com>
2024-09-07 19:18:01 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240907/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int x, y;
+    scanf("%d %d", &x, &y);
+    x+=8;
+    y*=3;
+
+    printf("%d\n%d\n%d", x, y, x*y);
+    return 0;
+}
