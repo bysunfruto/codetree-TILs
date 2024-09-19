bb598420fc59e3d2fd6c6acd31fffb01a2ba6ef6
bysunfruto <163089667+bysunfruto@users.noreply.github.com>
2024-09-19 13:57:13 +0900

    Title: 평균점수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240919/평균점수 구하기/print-average.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double arr[8];
+    double aver, sum;
+
+    for(int i = 0; i < 8 ; i++) {
+        scanf("%lf", &arr[i]);
+        sum += arr[i];
+    }
+
+    aver = sum / 8 ;
+    printf("%lf", aver);
+    return 0;
+}
